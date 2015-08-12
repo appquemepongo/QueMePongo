177a865fb1dc55fe39fa64f530f0ccdb6578b3b1
Matute <appquemepongo@gmail.com>
2015-08-12 20:18:56 -0300

    lo modifique

--- app/src/main/java/com/example/quemepongo/Main2Activity.java
+++ app/src/main/java/com/example/quemepongo/Main2Activity.java
@@ -1,50 +1,37 @@
 package com.example.quemepongo;
 
 import android.content.Intent;
-import android.os.CountDownTimer;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.TextView;
 
-
-public class logo_screen extends ActionBarActivity {
-
-    public static final int segundos=2;
-    public static final int milisegundos=segundos*1000;
+public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_logo_screen);
-        empezaranimacion();
-    }
+        setContentView(R.layout.activity_main2);
 
-    public void empezaranimacion()
-    {
-        new CountDownTimer(milisegundos,1000)
-        {
+        TextView textView = (TextView) findViewById(R.id.textView);
 
-            @Override
-            public void onTick(long millisUntilFinished) {
+        Intent intent = getIntent();
 
-            }
+        String genero = intent.getStringExtra("gender");
+        textView.setText(genero);
 
-            @Override
-            public void onFinish() {
-                Intent nuevoform=new Intent(logo_screen.this,mainActivity.class);
-                startActivity(nuevoform);
-                finish();
-            }
-        }.start();
+
 
     }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_logo_screen, menu);
+        getMenuInflater().inflate(R.menu.menu_main2, menu);
         return true;
     }
 
--- app/src/main/java/com/example/quemepongo/mainActivity.java
+++ app/src/main/java/com/example/quemepongo/mainActivity.java
@@ -1,18 +1,49 @@
 package com.example.quemepongo;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 
 public class mainActivity extends ActionBarActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageButton female  = (ImageButton) findViewById(R.id.female);
+        ImageButton male  = (ImageButton) findViewById(R.id.male);
+
+        female.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+                intent.putExtra("gender","femenino");
+                startActivity(intent);
+            }
+        });
+
+        male.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(),Main2Activity.class);
+                intent.putExtra("gender", "masculino");
+                startActivity(intent);
+            }
+        });
+
+
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
